f0d49df545a875ef742f4428a347677ddc64aa45
Miguel <miguelortiz8105@gmail.com>
2024-03-13 14:49:25 +0100

    'añadido texto diseñado footer'

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,24 +6,29 @@ export default function Footer({ id }: { id?: string }) {
   return (
     <footer
       id="contacto"
-      className={`flex h-32 w-full items-center justify-center bg-[#050505] px-2 py-5 text-[#909bac]`}
+      className={`flex w-full items-center justify-center bg-[#050505] px-2 py-5 text-[#909bac]`}
     >
-      <div className="mx-auto flex justify-center xl:container">
-        <Tooltip
-          content={<p className="bg-black text-orange-500">{textTooltip}</p>}
-          placement="bottom"
-        >
-          <p
-            className="m-0 text-xl font-medium hover:cursor-pointer hover:text-orange-500 md:text-2xl lg:text-3xl"
-            onClick={(e) => {
-              navigator.clipboard.writeText(e.currentTarget.innerText);
-              setTextTooltip("Copiado");
-            }}
-            onMouseLeave={() => setTextTooltip("Click para copiar")}
+      <div className="mx-auto w-full flex flex-col items-center justify-center xl:container">
+        <div className="my-10">
+          <Tooltip
+            content={<p className="bg-black text-orange-500">{textTooltip}</p>}
+            placement="bottom"
           >
-            miguelortiz8105@gmail.com
-          </p>
-        </Tooltip>
+            <p
+              className="m-0 text-xl font-medium hover:cursor-pointer hover:text-orange-500 md:text-2xl lg:text-3xl"
+              onClick={(e) => {
+                navigator.clipboard.writeText(e.currentTarget.innerText);
+                setTextTooltip("Copiado");
+              }}
+              onMouseLeave={() => setTextTooltip("Click para copiar")}
+            >
+              miguelortiz8105@gmail.com
+            </p>
+          </Tooltip>
+        </div>
+        <div className="flex justify-end w-full md:text-base text-xs">
+          <p>Diseñado por Miguel Ortiz</p>
+        </div>
       </div>
     </footer>
   );
